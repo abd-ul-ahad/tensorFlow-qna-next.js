ab1184107df807bd185be4a90ab5619244f14314
Abdul Ahad <abd-ul-ahad@outlook.com>
2023-07-16 16:51:07 +0500

    fixes

---
+++ client/out/_next/static/chunks/252f366e-bfbd18fc69583891.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,h,n){n.d(h,{x06:function(){return e}});var a=n(8357);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"}}]})(t)}}}]);
